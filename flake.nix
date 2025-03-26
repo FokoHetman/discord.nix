69b10c141f59c5a0ef15b60d1b9b86bad45338ca
FokoHetman <paprykkania@gmail.com>
2025-03-26 15:34:55 +0100

    flaek

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+
+    flake-utils = {
+      inputs.systems.follows = "systems";
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs =
+    {
+      nixpkgs,
+      self,
+      ...
+    }@inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        inherit (nixpkgs) lib;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+
+      })
+  // {
+    nixosModules.stylix =
+        { pkgs, ... }@args:
+        {
+          imports = [
+            (import ./. inputs)
+            {
+              discord = {
+                
+              };
+            }
+          ];
+        };
+  };
+}
