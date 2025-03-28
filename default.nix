5b27895c08e2ef51199e9d0fb20c2ed653d9a67e
FokoHetman <paprykkania@gmail.com>
2025-03-29 00:00:05 +0100

    perms

--- default.nix
+++ default.nix
@@ -94,6 +94,7 @@ let
     libraries = with pkgs.python3Packages; [ requests json5 ];
     flakeIgnore = ["E111" "E114" "E251" "E128" "E201" "E202" "W291" "W293" "E265" "E501" "E231" "E261" "E225"];
   } /*python3*/ ''
+
 import requests
 import json
 
@@ -111,9 +112,99 @@ headers = {"Authorization": token, "Content-Type": "application/json"}
 
 guilds = requests.get('https://discord.com/api/users/@me/guilds', headers=headers).json()
 
+def camel_case(text):
+    s = text.lower().replace("-", " ").replace("_", " ")
+    s = s.split()
+    if len(text) == 0:
+        return text
+    return s[0] + ''\''.join(i.capitalize() for i in s[1:])
+
+perms = map(camel_case, [
+  "CREATE_INSTANT_INVITE",
+  "KICK_MEMBERS",
+  "BAN_MEMBERS",
+  "ADMINISTRATOR",
+  "MANAGE_CHANNELS",
+  "MANAGE_GUILD",
+  "ADD_REACTIONS",
+  "VIEW_AUDIT_LOG",
+  "PRIORITY_SPEAKER",
+  "STREAM",
+  "VIEW_CHANNEL",
+  "SEND_MESSAGES",
+  "SEND_TTS_MESSAGES",
+  "MANAGE_MESSAGES",
+  "EMBED_LINKS",
+  "ATTACH_FILES",
+  "READ_MESSAGE_HISTORY",
+  "MENTION_EVERYONE",
+  "USE_EXTERNAL_EMOJIS",
+  "VIEW_GUILD_INSIGHTS",
+  "CONNECT",
+  "SPEAK",
+  "MUTE_MEMBERS",
+  "DEAFEN_MEMBERS",
+  "MOVE_MEMBERS",
+  "USE_VAD",
+  "CHANGE_NICKNAME",
+  "MANAGE_NICKNAMES",
+  "MANAGE_ROLES",
+  "MANAGE_WEBHOOKS",
+  "MANAGE_EMOJIS_AND_STICKERS",
+  "USE_APPLICATION_COMMANDS",
+  "REQUEST_TO_SPEAK",
+  "MANAGE_EVENTS",
+  "MANAGE_THREADS",
+  "CREATE_PUBLIC_THREADS",
+  "CREATE_PRIVATE_THREADS",
+  "USE_EXTERNAL_STICKERS",
+  "SEND_MESSAGES_IN_THREADS",
+  "USE_EMBEDDED_ACTIVITIES", 
+  "MODERATE_MEMBERS",
+  "VIEW_CREATOR_MONETIZATION_ANALYTICS",
+  "USE_SOUNDBOARD",
+  "CREATE_GUILD_EXPRESSIONS",
+  "CREATE_EVENTS",
+  "USE_EXTERNAL_SOUNDS",
+  "SEND_VOICE_MESSAGES",
+  "SEND_POLLS",
+  "USE_EXTERNAL_APPS"
+])
+
+def build_permissions(discord_roles, roles, users):
+  # iterate users too
+  res = []
+  for i in roles[:-1]:
+    role_id = -1
+    if i.isnumeric():
+      role_id = i
+    else:
+      for j in discord_roles:
+        if j["name"]==i:
+          role_id = j["id"]
+    if role_id<0:
+      raise Exception("non existent role")
+    
+    allow = 0
+    deny = 0
+    for permission in roles[i]:
+      if roles[i][permission]:
+        allow |= (1 << perms.index(permission))
+      elif roles[i][permission]==False:
+        deny  |= (1 << perms.index(permission))
+
+    res = {}
+    res["id"] = role_id
+    res["type"] = 0
+    res["allow"] = allow
+    res["den"] = deny
+    result.append(res)
+  return result
+
 for i in guilds:
   if i["name"] in config["servers"]: # add id checking too later, for name redefining
     chctg = requests.get(f"https://discord.com/api/guilds/{i['id']}/channels", headers=headers).json()
+    roles = requests.get(f"https://discord.com/api/guilds/{i['id']}/roles", headers=headers).json()
     print(chctg)
     categories = []
     channels = []
@@ -131,9 +222,22 @@ for i in guilds:
       id = 0
       if category not in map(lambda x: x["name"], categories):
         print("Creating category: ", category)
+
+
+        rolec = {}
+        userc = {}
+        cut = config["servers"][i["name"]]["categories"][category]["permissions"]
+        if "roles" in cut:
+          rolec = cut["roles"]
+        if "users" in cut:
+          userc = cut["users"]
+
+        overwrites = build_permissions(roles, rolec,
+                                              userc)
+
         
         resp = requests.post(f"https://discord.com/api/guilds/{i['id']}/channels", 
-          json = {"name": category, "type": 4}, headers=headers).json()
+          json = {"name": category, "type": 4, "permission_overwrites": overwrites}, headers=headers).json()
         id = resp["id"]
         # print(resp)
         print(f"Created {category} with ID: ", resp["id"])
@@ -147,12 +251,25 @@ for i in guilds:
         if channel not in map(lambda x: x["name"], filter( lambda x: x["parent_id"]==id, channels ) ):
           print("Creating channel: ", channel)
 
+          rolec = {}
+          userc = {}
+          cut = config["servers"][i["name"]]["categories"][category]["channels"]["permissions"]
+          if "roles" in cut:
+            rolec = cut["roles"]
+          if "users" in cut:
+            userc = cut["users"]
+
+          overwrites = build_permissions(roles, rolec,
+                                                userc)
+
           resp = requests.post(f"https://discord.com/api/guilds/{i['id']}/channels",
-            json = {"name": channel, "type": 0, "parent_id": id}, headers=headers).json()
+            json = {"name": channel, "type": 0, "parent_id": id, "permission_overwrites": overwrites}, headers=headers).json()
 
       # for channel in config["channels"][i["name"]]["categories"][category]["channels"]:
       #   pass
-print("discord.nix: Done.")
+
+
+
 '';
 
   
